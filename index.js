13de51148e715b8cf38fd24dcab4fea04c106e41
JOE MON <56827756+JOEMN96@users.noreply.github.com>
2021-09-18 13:06:57 +0530

    recursive progress day 1

--- index.js
+++ index.js
@@ -1,80 +1,156 @@
 import express from "express";
 import puppeteer from "puppeteer";
-import dotenv from "dotenv"
-import * as cheerio from 'cheerio';
-import axios from 'axios'
-import request from "request"
+import dotenv from "dotenv";
+import * as cheerio from "cheerio";
+import axios from "axios";
+import request from "request";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(express.json())
+app.use(express.json());
 
+// const getImages = async (url) => {
+//   // const {data} = await  axios.get(url)
+//   if(url.endsWith('/')) {
+//     url = url.slice(0, -1)
+//   }
+//   console.log("Loading");
+//   let rooturl = url
+//   let pages = []
+//   let images =[]
+//   let iteration = 0
 
+//   async function scrape (url)  {
+//     request(url, (err, resp, html)=>{
 
+//       console.log(err);
 
+//     if(!err && resp.statusCode == 200){
+
+//       let $ = cheerio.load(html)
+//       $('a').each((index,item) => {
+
+//         if(!item.attribs.href) {
+//           return
+//         }
+
+//         if(item.attribs.href.startsWith(rooturl)) {
+//           if(!images.includes(item.attribs.href)) {
+//               images.push(item.attribs.href)
+//           }
+//         } else if  (item.attribs.href.startsWith('/')) {
+//           images.push(rooturl+ item.attribs.href)
+//         }
+//       })
+//         // console.log(images);
+
+//         $("img").each((index, image)=>{
+//           var img = $(image).attr('src');
+//           var Links = img;
+//           images.push(Links);
+//         });
+//         iteration ++
+//         console.log(iteration);
+//         if(pages[iteration]) {
+//           scrape(pages[iteration])
+//         } else {
+//           console.log('fired');
+//           return images
+//         }
+//         // console.log(iteration);
+//       }
+//     })
+//   }
+//  scrape(url)
+
+// }
+
+Set.prototype.getByIndex = function (index) {
+  return [...this][index];
+};
 
 const getImages = async (url) => {
-  // const {data} = await  axios.get(url)
-  if(url.endsWith('/')) {
-    url = url.slice(0, -1)
+  if (url.endsWith("/")) {
+    url = url.slice(0, -1);
   }
   console.log("Loading");
+  let rooturl = url;
+  let pages = new Set();
+  pages.add(url);
+  let images = [];
+  let iteration = 0;
 
-  let pages = []
-  let images =[]
+  try {
+    const val = await scrape(url);
+  } catch (e) {
+    console.log(e.message);
+    if (pages[iteration]) {
+      iteration++;
+      pages[iteration];
+    }
+  }
 
-  function scrape (url)  {
-    request(url, (err, resp, html)=>{
-
-      if(!err && resp.statusCode == 200){
-
-        if(images.includes(url)) {
-          return scrape()
+  async function scrape(url) {
+    try {
+      const { data } = await axios.get(url);
+      let $ = cheerio.load(data);
+      $("a").each((index, item) => {
+        if (!item.attribs.href) {
+          return;
         }
 
-        let $ = cheerio.load(html)
+        if (item.attribs.href.startsWith(rooturl)) {
+          return pages.add(item.attribs.href);
+        } else if (item.attribs.href.startsWith("/")) {
+          return pages.add(rooturl + item.attribs.href);
+        }
+      });
 
-        let atags = $('a').each((index,item) => {
-          if(item.attribs.href == "" ) {
-            return
-          } else if (item.attribs.href.startsWith('/')) {
-            pages.push(url+item.attribs.href)
-          }
-        })
+      $("img").each((index, image) => {
+        var img = $(image).attr("src");
+        images.push(img);
+      });
 
-        $("img").each((index, image)=>{
-          var img = $(image).attr('src');
-          var Links = url + img;
-          images.push(Links);
-        });
-        console.log(images);                                  
+      console.log(images);
+
+      iteration++;
+      if (pages.getByIndex(iteration)) {
+        scrape(pages.getByIndex(iteration));
+      } else {
+        console.log("Done");
+        return images;
       }
-    })
+    } catch (error) {
+      console.log(error.message);
+      iteration++;
+      if (pages.getByIndex(iteration)) {
+        scrape(pages.getByIndex(iteration));
+      } else {
+        console.log(iteration);
+        console.log("From CAtch block scrape");
+        return images;
+      }
+    }
   }
-  scrape(url)
+};
 
-
-}
-
-await getImages('https://www.blockonomics.co/')
-
+await getImages("https://www.blockonomics.co/");
 
 // app.get("/api", async (req, res) => {
 //   const {url} = req.query;
 //   if(!url) return res.status(400).send()
 //   const images = await getImages(url);
+//   console.log(images);
 //   if(images == undefined || images.length < 1)  return res.status(404).send()
 //   res.status(200).send({images})
 // });
 
-
-app.listen(process.env.PORT || 2000, () => {
-  console.log("App is running ");
-});
-
+// app.listen(process.env.PORT || 2000, () => {
+//   console.log("App is running ");
+// });
 
 // try {
 //   const browser = await puppeteer.launch({ headless: true,args: ['--no-sandbox'] });
