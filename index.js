54eb4913ca779e44bdb75b25ec900951e198ae7c
JOE MON <56827756+JOEMN96@users.noreply.github.com>
2021-09-18 23:27:40 +0530

    Code refactoring

--- index.js
+++ index.js
@@ -10,14 +10,10 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 
-// let images = await getImages("https://www.blockonomics.co/");
-// console.log(images);
-
 app.get("/api", async (req, res) => {
   const { url } = req.query;
   if (!url) return res.status(400).send();
   const images = await getImages(url);
-  console.log(images);
   if (images == undefined || images.length < 1) return res.status(404).send();
   res.status(200).send({ images });
 });
